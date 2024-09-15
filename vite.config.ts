d6d67af47bb69ddb37177cec9db17b1b8e9b12f6
Juancapp <juan.cappuccio@radiumrocket.com>
2024-09-15 20:34:52 -0300

    m

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/fonts/GABRWFFR.ttf',
-          dest: '/fonts'
-        }
-      ]
-    })
+    react()
   ],
   resolve: {
     alias: {
@@ -24,4 +15,5 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
   },
+  base: "/vite-react-deploy/",
 });
